09b0461969cbf14bc44893f08bee459f42020d43
Rajesh <rajeshchandran007@gmail.com>
2024-12-03 14:09:29 +0000

    day 1 updates

--- day-1/day-1-solutions.sh
+++ day-1/day-1-solutions.sh
@@ -1,4 +1,6 @@
+#------------------------------------------------#
 # Task 1: Comments
+#------------------------------------------------#
 #This is Single line comment
 << cmt
 This is a multi-line comment.
@@ -7,15 +9,18 @@ cmt
 echo "Task 1 Completed."
 #------------------------------------------------#
 # Task 2: Echo
+#------------------------------------------------#
 echo "Task 2 Completed."
 #------------------------------------------------#
 # Task 3: Variables
+#------------------------------------------------#
 v1="Hello"
 v2="Rajesh"
 echo $v1 $v2
 echo "Task 3 Completed."
 #------------------------------------------------#
 # Task 4: Using Variables
+#------------------------------------------------#
 read -p "Enter number 1: " num1
 read -p "Enter number 2: " num2
 sum=$(expr $num1 + $num2)
@@ -23,6 +28,7 @@ echo "Sum of $num1 and $num2 is: $sum"
 echo "Task 4 Completed."
 #------------------------------------------------#
 # Task 5: Using Built-in Variables
+#------------------------------------------------#
 echo "USER - $USER"
 echo "PWD - $PWD"
 echo "HOME - $HOME"
@@ -31,6 +37,6 @@ echo "Host Name - $HOSTNAME"
 echo "Task 5 Completed"
 #------------------------------------------------#
 # Task 6: Wildcards
+#------------------------------------------------#
 ls -l *.sh
 echo "Task 6 Completed"
-#------------------------------------------------#
